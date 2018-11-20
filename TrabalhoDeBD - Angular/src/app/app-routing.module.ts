ba9068a3a1254654f6f47dd78bbd3a12f86fb2e5
Yasmin <ymlima951@gmail.com>
2018-11-20 19:52:55 -0200

    Update 2 20.11.18

--- TrabalhoDeBD - Angular/src/app/app-routing.module.ts
+++ TrabalhoDeBD - Angular/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GamesComponent } from './games/games.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PackagesComponent } from './packages/packages.component';
+import { UsersComponent } from './users/users.component';
+import { TagsComponent } from './tags/tags.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'games',
+    component: GamesComponent
+  },
+  {
+    path: 'companies',
+    component: CompaniesComponent
+  },
+  {
+    path: 'packages',
+    component: PackagesComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'tags',
+    component: TagsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TrabalhoDeBD - Angular/src/app/app.module.ts
+++ TrabalhoDeBD - Angular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { GamesComponent } from './games/games.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PackagesComponent } from './packages/packages.component';
+import { UsersComponent } from './users/users.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    HomeComponent,
+    GamesComponent,
+    CompaniesComponent,
+    PackagesComponent,
+    UsersComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
