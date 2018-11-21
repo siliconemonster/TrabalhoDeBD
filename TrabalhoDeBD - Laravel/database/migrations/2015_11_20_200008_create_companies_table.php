5021bfd554e6f54b5ca10b4eac5322c5ce0dede6
Aline F Rezende <alinefrezende@dcc.ufrj.br>
2018-11-21 03:27:46 -0200

    +todas as tables

--- TrabalhoDeBD - Laravel/database/migrations/2015_11_20_200008_create_companies_table.php
+++ TrabalhoDeBD - Laravel/database/migrations/2015_11_20_200008_create_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('userID');
-            $table->string('username')->unique();
-            $table->integer('level');
-            $table->date('accountTime');
-            $table->string('profilePic');
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('compID');
+            $table->string('compName');
+            $table->string('basedIn_state');
+            $table->string('basedIn_country');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
     }
 }
