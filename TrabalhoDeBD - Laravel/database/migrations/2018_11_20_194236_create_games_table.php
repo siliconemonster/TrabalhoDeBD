dc813b114db372637e459736f20a1ff53b659d81
Aline F Rezende <alinefrezende@dcc.ufrj.br>
2018-11-21 02:04:43 -0200

    +angular

---
+++ TrabalhoDeBD - Laravel/database/migrations/2018_11_20_194236_create_games_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('gameID');
+            $table->string('gameName');
+            $table->date('releaseDate');
+            $table->float('gamePrice');
+            $table->integer('amountOfReviews');
+            $table->float('rating');
+            $table->integer('fk_Developer_Company_compID')->nullable();
+            $table->integer('fk_Publisher_Company_compID')->nullable();
+            $table->string('odbc_tableprivileges');
+            $table->timestamps();
+        });
+
+        Schema::table('games', function (Blueprint $table) {
+           $table->foreign('fk_Developer_Company_compID')->references('id')->on('companies');
+           $table->foreign('fk_Publisher_Company_compID')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
--- TrabalhoDeBD - Laravel/database/migrations/2018_11_20_195159_create_packages_table.php
+++ TrabalhoDeBD - Laravel/database/migrations/2018_11_20_195159_create_packages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePackagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('userID');
-            $table->string('username');
-            $table->integer('level');
-            $table->date('accountTime');
-            $table->binary('profilePic');
+        Schema::create('packages', function (Blueprint $table) {
+            $table->increments('packID');
+            $table->string('packName');
+            $table->date('packReleaseDate');
+            $table->float('packPrice');
+            $table->boolean('packSatus');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('packages');
     }
 }
