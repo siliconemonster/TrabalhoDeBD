c744cc8a3841e155205e06adc2aa251eda447535
Aline <alinefrez@netscape.net>
2018-11-21 18:52:26 -0200

    +Controller de Game, Company, Package, User

--- TrabalhoDeBD - Laravel/app/Http/Controllers/UserController.php
+++ TrabalhoDeBD - Laravel/app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+      $user = User::all();
+      return $user->toJson();
     }
 
     /**
@@ -25,7 +26,30 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $novoUser = new User;
+      $novoUser->username = $request->username;
+      $novoUser->level = $request->level;
+      $novoUser->accountTime = $request->accountTime;
+      $novoUser->save();
+
+      //lógica para o upload de imagens.
+      if(!Storage::exists('localPhotos/'))
+      {
+        Storage::makeDirectory('localPhotos/', 0775, true);
+      }
+      //decodifica a string em base64 e a atribui a uma variável
+      $image = base64_decode($request->profilePic);
+      //gera um nome único para o arquivo e concatena seu nome com a
+      //extensão ‘.png’ para termos de fato uma imagem
+      $imgName = uniqid() . '.png';
+      //atribui a variável o caminho para a imagem que é constituída do
+      //caminho das pastas e o nome do arquivo
+      $path = storage_path('/app/localPhotos/'.$imgName);
+      //salva o que está na variável $image como o arquivo definido em $path
+      file_put_contents($path,$image);
+      $this->profilePic = $imgName;
+
+      $this->save();
     }
 
     /**
@@ -36,7 +60,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findorfail($userID);
+        return $user->toJson();
     }
 
     /**
@@ -46,10 +71,23 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+     public function update(Request $request, $id)
+     {
+       $user = User::findorfail($userID);
+
+        if($request->username){
+          $user->username = $request->username;
+        }
+        if($request->level){
+          $user->level = $request->level;
+        }
+        if($request->accountTime){
+            $user->accountTime = $request->accountTime;
+        }
+
+         $user->save();
+         return $user->toJson();
+      }
 
     /**
      * Remove the specified resource from storage.
@@ -57,8 +95,8 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($userID)
     {
-        //
+      User::destroy($userID);
     }
 }
