9523ebdd4d76fb3aade086dfa446234618193d9b
Gilberto Lopes <gilbertolopesif@ufrj.br>
2018-11-21 22:59:32 -0200

    relações definidas nas models
    mais pequenas correções

--- TrabalhoDeBD - Laravel/app/Models/Company.php
+++ TrabalhoDeBD - Laravel/app/Models/Company.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $table = 'companies';
+    protected $primarykey = 'compID';
+
+    public function publishes () {
+        return $this->hasMany('App\Models\Game', 'fk_Publisher_Company_compID', 'compID');
+    }
+
+    public function develops () {
+        return $this->hasMany('App\Models\Game', 'fk_Developer_Company_compID', 'compID');
+    }
 }
--- TrabalhoDeBD - Laravel/app/Models/Game.php
+++ TrabalhoDeBD - Laravel/app/Models/Game.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    protected $primarykey = 'gameID';
+
+    public function packages()
+    {
+        return $this->belongsToMany('App\Models\Package', 'game_package', 'gameID', 'packID');
+    }
+
+    public function published_by () {
+        return $this->belongsTo('App\Models\Company', 'fk_Publisher_Company_compID', 'compID');
+    }
+
+    public function developed_by () {
+        return $this->belongsTo('App\Models\Company', 'fk_Developer_Company_compID', 'compID');
+    }
+
+    public function owned_by() {
+        return $this->belongsToMany('App\Models\User', 'games_owned', 'gameID', 'userID');
+    }
+
+    public function played_by() {
+        return $this->belongsToMany('App\Models\User', 'games_played', 'gameID', 'userID');
+    }
 }
